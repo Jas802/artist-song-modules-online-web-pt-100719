63d3ad02fb4b9413d9de327c4186db20c2fb7db3
Jas Spencer <jas.spencer414@gmail.com>
2019-11-10 00:04:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,13 +22,6 @@ class Artist
     @@artists
   end
 
-#  def self.reset_all
-#    self.all.clear
-#  end
-
-#  def self.count
-#    self.all.count
-#  end
 
   def add_song(song)
     @songs << song
@@ -39,7 +32,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
